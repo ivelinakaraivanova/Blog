4fb24f6ab1926efdb69ec0159747ed85987998d4
ivelinakaraivanova <73067985+ivelinakaraivanova@users.noreply.github.com>
2021-11-17 10:47:59 +0200

    Update user profile added

--- src/blog/blog/accounts_app/views.py
+++ src/blog/blog/accounts_app/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from blog.accounts_app.forms import UserRegisterForm
+from blog.accounts_app.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -23,4 +23,23 @@ def register(request):
 
 @login_required()
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    if request.method == "POST":
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': user_update_form,
+        'p_form': profile_update_form,
+    }
+
+    return render(request, 'accounts/profile.html', context)
